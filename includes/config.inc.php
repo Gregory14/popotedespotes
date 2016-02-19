0766bfb3cb22bd1249d60bf128f35c45a5770a2c
qhuit <qhuit@free.fr>
2016-02-19 17:23:40 +0100

    add : Frontend dev homepage

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 define('IMG_HIRES_URI', '/img/hires/');
-define('IMG_HIRES_PATH', '~gregoryjoly/Applications/MAMP/htdocs'. IMG_HIRES_URI);
+define('IMG_HIRES_PATH', '/Applications/MAMP/htdocs'. IMG_HIRES_URI);
 define('IMG_THUMBS_WIDTH', 160);
 define('IMG_THUMBS_URI', '/img/thumbs/');
-define('IMG_THUMBS_PATH', '~gregoryjoly/Applications/MAMP/htdocs/'. IMG_THUMBS_URI);
+define('IMG_THUMBS_PATH', '/Applications/MAMP/htdocs/'. IMG_THUMBS_URI);
 
 define('DEBUG', 0);
---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(function(){
+    $(document).scroll(function(){
+        var $scroll = $(window).scrollTop();
+        $('figure img').css('transform',"translateY(" +  ($scroll * 0.4) + "px)");
+        if($(this).scrollTop() > 40) {
+            $('header').addClass('fixedHeader');
+            if ($(this).scrollTop() > 785) {
+                $('header').addClass('whiteHeader');
+            } else {
+                $('header').removeClass('whiteHeader');
+            }
+        } else {
+            $('header').removeClass('fixedHeader');
+            //$('figure img').css('transform',"translateY(0px)");
+        }
+    })
+});
