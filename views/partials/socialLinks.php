e5f0878519b9cf2b57896f3f5395b1053796501a
qhuit <qhuit@free.fr>
2016-02-23 19:32:20 +0100

    fix : fomulaire cours

--- views/partials/socialLinks.php
+++ views/partials/socialLinks.php
@@ -6,8 +6,41 @@
  * Time: 19:07
  */;?>
 
-<ul>
-    <li>Linkedin</li>
-    <li>facebook</li>
-    <li>twitter</li>
+<ul class="socialLinks">
+    <li>
+        <!-- Generator: Adobe Illustrator 19.2.0, SVG Export Plug-In  -->
+       <a href="http://www.linkedin.com" target="_blank"> <svg version="1.1"
+             xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"
+             x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve">
+<defs>
+</defs>
+            <path d="M12,0C5.4,0,0,5.4,0,12s5.4,12,12,12s12-5.4,12-12S18.6,0,12,0z M10,16H8v-6h2V16z M9,9.1C8.4,9.1,7.9,8.6,7.9,8
+	c0-0.6,0.5-1.1,1.1-1.1s1.1,0.5,1.1,1.1C10.1,8.6,9.6,9.1,9,9.1z M17,16h-2v-2.9c0-1.9-2-1.7-2,0V16h-2v-6h2v1.1
+	c0.9-1.6,4-1.7,4,1.5V16z"/>
+</svg>
+       </a>
+    </li>
+    <li><!-- Generator: Adobe Illustrator 19.2.0, SVG Export Plug-In  -->
+
+        <a href="http://www.facebook.com" target="_blank">  <svg version="1.1"
+             xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"
+             x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve">
+<defs>
+</defs>
+            <path d="M12,0C5.4,0,0,5.4,0,12s5.4,12,12,12s12-5.4,12-12S18.6,0,12,0z M15,8h-1.4C13.1,8,13,8.2,13,8.8V10h2l-0.2,2H13v7h-3v-7H8
+	v-2h2V7.7C10,5.9,10.9,5,13,5h2V8z"/>
+</svg>
+    </li>
+    <li><!-- Generator: Adobe Illustrator 19.2.0, SVG Export Plug-In  -->
+        <a href="http://www.twitter.com" target="_blank"><svg version="1.1"
+             xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"
+             x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve">
+<defs>
+</defs>
+            <path d="M12,0C5.4,0,0,5.4,0,12s5.4,12,12,12s12-5.4,12-12S18.6,0,12,0z M18.1,9.6c0.2,4-2.8,8.5-8.2,8.5c-1.6,0-3.1-0.5-4.4-1.3
+	c1.5,0.2,3-0.2,4.3-1.2c-1.3,0-2.3-0.9-2.7-2c0.5,0.1,0.9,0.1,1.3,0C7,13.4,6,12.1,6.1,10.8c0.4,0.2,0.8,0.3,1.3,0.4
+	c-1.3-0.9-1.6-2.5-0.9-3.8c1.4,1.7,3.5,2.9,5.9,3c-0.4-1.8,0.9-3.5,2.8-3.5c0.8,0,1.6,0.3,2.1,0.9c0.7-0.1,1.3-0.4,1.8-0.7
+	c-0.2,0.7-0.7,1.2-1.3,1.6c0.6-0.1,1.1-0.2,1.6-0.5C19.1,8.7,18.6,9.2,18.1,9.6z"/>
+</svg>
+    </li>
 </ul>
